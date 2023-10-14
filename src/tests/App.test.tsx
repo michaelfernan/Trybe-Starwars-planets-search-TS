c4b6946fe3bba6966d941352f8f88c98b245ccc5
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-10-14 19:03:15 -0300

    ...

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
-import { PlanetProvider } from '../components/PlanetContext';
 
-test('Renders the table header', () => {
-  const { getByText } = render(
-    <PlanetProvider>
-      <App />
-    </PlanetProvider>
-  );
-
-  const tableTitle = getByText('Tabela de Planetas de Star Wars');
-  expect(tableTitle).toBeInTheDocument();
+test('I am your test', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello, App!/i);
+  expect(linkElement).toBeInTheDocument();
 });
